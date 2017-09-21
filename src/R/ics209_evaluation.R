8959397a401adfe45c19ebead9c2508e8dbb8c4b
Your Name <natemietkiewicz@gmail.com>
2017-09-21 15:14:26 -0600

    new exploratory plots

--- src/R/ics209_evaluation.R
+++ src/R/ics209_evaluation.R
@@ -36,38 +36,64 @@ p1 <- wui209_shp %>%
   #filter(cause == "Human") %>%
   filter(cause != "Unk") %>%
   ggplot(aes(x = (costs))) +
-  geom_point(aes(y = max.aerial, color = cause), size = 0.75) +
-  geom_smooth(aes(y = max.aerial, color = cause), method="glm", method.args = list(family = "poisson"),
+  geom_point(aes(y = log(max.aerial), color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(max.aerial), color = cause), method="glm", method.args = list(family = "poisson"),
               se= TRUE, size = 0.75) +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
-  xlab("")  + ylab("Max aerial") +
-  theme_pub() +facet_wrap(~Class, scales = "free")  
+  xlab("")  + ylab("log(Max aerial)") +
+  theme_pub() +facet_wrap(~Class)  
 
 p2 <- wui209_shp %>%
   transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
   #filter(cause == "Human") %>%
   filter(cause != "Unk") %>%
   ggplot(aes(x = (costs))) +
-  geom_point(aes(y = max.pers, color = cause), size = 0.75) +
-  geom_smooth(aes(y = max.pers, color = cause), method="glm", method.args = list(family = "poisson"),
+  geom_point(aes(y = log(max.pers), color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(max.pers), color = cause), method="glm", method.args = list(family = "poisson"),
               se= TRUE, size = 0.75) +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
-  xlab("") + ylab("Max personnel") +
-  theme_pub() +facet_wrap(~Class, scales = "free")  
+  xlab("") + ylab("log(Max personnel)") +
+  theme_pub() +facet_wrap(~Class)  
 
 p3 <- wui209_shp %>%
   transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
   #filter(cause == "Human") %>%
   filter(cause != "Unk") %>%
   ggplot(aes(x = (costs))) +
-  geom_point(aes(y = max.agency.support, color = cause), size = 0.75) +
-  geom_smooth(aes(y = max.agency.support, color = cause), method="glm", method.args = list(family = "poisson"),
+  geom_point(aes(y = log(max.agency.support), color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(max.agency.support), color = cause), method="glm", method.args = list(family = "poisson"),
               se= TRUE, size = 0.75) +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
-  xlab("log suppression costs") + ylab("Max agency support") +
-  theme_pub() +facet_wrap(~Class, scales = "free")  
+  xlab("") + ylab("log(Max agency support)") +
+  theme_pub() +facet_wrap(~Class)  
 
-grid.arrange(p1, p2, p3, nrow =3)
+p4 <- wui209_shp %>%
+  transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
+  #filter(cause == "Human") %>%
+  filter(cause != "Unk") %>%
+  ggplot(aes(x = (costs))) +
+  geom_point(aes(y = log(home.destroyed)+1, color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(home.destroyed)+1, color = cause), method="glm", method.args = list(family = "poisson"),
+              se= TRUE, size = 0.75) +
+  scale_color_manual(values = c("#1F77B4", "#D62728")) +
+  xlab("") + ylab("log(homes destroyed) + 1") +
+  theme_pub() +facet_wrap(~Class)  
+
+p5 <- wui209_shp %>%
+  transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
+  #filter(cause == "Human") %>%
+  filter(cause != "Unk") %>%
+  ggplot(aes(x = (costs))) +
+  geom_point(aes(y = log(home.threat)+1, color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(home.threat)+1, color = cause), method="glm", method.args = list(family = "poisson"),
+              se= TRUE, size = 0.75) +
+  scale_color_manual(values = c("#1F77B4", "#D62728")) +
+  xlab("log(suppression costs)") + ylab("log(homes threatened) + 1") +
+  theme_pub() +facet_wrap(~Class)  
+
+grid.arrange(p1, p2, p3, p4, p5, nrow =5)
+
+
 
 
 
@@ -75,44 +101,104 @@ t1 <- wui209_shp %>%
   transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
   #filter(cause == "Human") %>%
   filter(cause != "Unk") %>%
-  ggplot(aes(x = area_km2)) +
-  geom_point(aes(y = max.aerial, color = cause), size = 0.75) +
-  geom_smooth(aes(y = max.aerial, color = cause), method="glm", method.args = list(family = "poisson"),
+  ggplot(aes(x = log(area_km2))) +
+  geom_point(aes(y = log(max.aerial), color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(max.aerial), color = cause), method="glm", method.args = list(family = "poisson"),
               se= TRUE, size = 0.75) +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
-  xlab("")  + ylab("Max aerial") +
+  xlab("")  + ylab("log(Max aerial)") +
   theme_pub() +facet_wrap(~Class, scales = "free")  
 
 t2 <- wui209_shp %>%
   transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
   #filter(cause == "Human") %>%
   filter(cause != "Unk") %>%
-  ggplot(aes(x = area_km2)) +
-  geom_point(aes(y = max.pers, color = cause), size = 0.75) +
-  geom_smooth(aes(y = max.pers, color = cause), method="glm", method.args = list(family = "poisson"),
+  ggplot(aes(x = log(area_km2))) +
+  geom_point(aes(y = log(max.pers), color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(max.pers), color = cause), method="glm", method.args = list(family = "poisson"),
               se= TRUE, size = 0.75) +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
-  xlab("") + ylab("Max personnel") +
+  xlab("") + ylab("log(Max personnel)") +
   theme_pub() +facet_wrap(~Class, scales = "free")  
 
 t3 <- wui209_shp %>%
   transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
   #filter(cause == "Human") %>%
   filter(cause != "Unk") %>%
-  ggplot(aes(x = area_km2)) +
-  geom_point(aes(y = max.agency.support, color = cause), size = 0.75) +
-  geom_smooth(aes(y = max.agency.support, color = cause), method="glm", method.args = list(family = "poisson"),
+  ggplot(aes(x = log(area_km2))) +
+  geom_point(aes(y = log(max.agency.support)+1, color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(max.agency.support)+1, color = cause), method="glm", method.args = list(family = "poisson"),
               se= TRUE, size = 0.75) +
   scale_color_manual(values = c("#1F77B4", "#D62728")) +
-  xlab("Burned area (km2)") + ylab("Max agency support") +
+  xlab("") + ylab("log(Max agency support)") +
   theme_pub() +facet_wrap(~Class, scales = "free")  
 
-grid.arrange(t1, t2, t3, nrow =3)
+t4 <- wui209_shp %>%
+  transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
+  #filter(cause == "Human") %>%
+  filter(cause != "Unk") %>%
+  ggplot(aes(x = log(area_km2))) +
+  geom_point(aes(y = log(home.destroyed)+1, color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(home.destroyed)+1, color = cause), method="glm", method.args = list(family = "poisson"),
+              se= TRUE, size = 0.75) +
+  scale_color_manual(values = c("#1F77B4", "#D62728")) +
+  xlab("") + ylab("log(homes destroyed) + 1") +
+  theme_pub() +facet_wrap(~Class, scales = "free")  
+
+t5 <- wui209_shp %>%
+  transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
+  #filter(cause == "Human") %>%
+  filter(cause != "Unk") %>%
+  ggplot(aes(x = log(area_km2))) +
+  geom_point(aes(y = log(home.threat)+1, color = cause), size = 0.75, alpha = 0.25, shape = 16) +
+  geom_smooth(aes(y = log(home.threat)+1, color = cause), method="glm", method.args = list(family = "poisson"),
+              se= TRUE, size = 0.75) +
+  scale_color_manual(values = c("#1F77B4", "#D62728")) +
+  xlab("log(area_km2)") + ylab("log(homes threatened) + 1") +
+  theme_pub() +facet_wrap(~Class, scales = "free")  
+
+grid.arrange(t1, t2, t3, t4, t5, nrow =5)
 
 
 
+library(mblm)
+fit1 <- mblm(costs ~ max.pers, data = wui209_shp)
+
+library(agricolae)
+t <- as.data.frame(wui209_shp)
+for_ts <- t %>%
+  filter(cause != "Unk") %>%
+  mutate(combo = paste(cause, Class, sep = "_")) %>%
+  select(combo, area_km2, fatalities, home.destroyed, home.threat, max.aerial, tot.aerial,
+         max.pers, tot.pers, max.agency.support) %>%
+  gather(variable, value, -combo, -area_km2) %>%
+  split(.$variable) %>%
+  map(~  mblm(area_km2 ~ value + combo, data = .x))
+  # map(~ aov(area_km2 ~ combo + value, data = .x)) %>%
+  # map(HSD.test, trt = 'combo', alpha = 0.05)
+  
+aov.models <- wui209_shp %>%
+  select(-Year, -Age) %>%
+  gather(variable, value, -sb_age, -Site, -Plot) %>%
+  split(.$variable) %>%
+  map(~  mblm(variable ~ max.pers + varaible, data = .x))
+  #map(~ aov(value ~ sb_age + Site/Plot, data = .x)) %>%
+  #map(HSD.test, trt = 'sb_age', alpha = 0.05)
 
 
+plot(fit1)
+residuals(fit1)
+fitted(fit1)
+plot(density(fit1$slopes))
+plot(density(fit1$intercepts))
+anova(fit1)
+
+
+
+anova(fit2)
+anova(fit,fit2)
+confint(fit)
+AIC(fit,fit2)
 
 
 
@@ -143,12 +229,14 @@ grid.arrange(t1, t2, t3, nrow =3)
 
 
 t <- wui209_shp %>%
+  filter(incidentnum == "CA-KNF-3393") %>%
   filter(cause != "Unk") %>%
-  group_by(syear, cause) %>%
+  group_by(Class, syear, cause) %>%
   summarise(costs = sum(as.numeric(costs)),
             area_km2 = sum(as.numeric(area_km2)))
 
 ics_sums_p <- t %>%
+  transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
   ggplot(aes(x = syear)) +
   geom_point(aes(y = costs/100000000, color = cause), size = 2) +
   geom_line(aes(y = costs/100000000, color = cause),  size = 0.5, alpha = 0.25) +
@@ -159,7 +247,8 @@ ics_sums_p <- t %>%
   theme_pub()  + 
   theme(axis.title = element_text(face = "bold"),
         strip.text = element_text(size = 10, face = "bold"),
-        legend.position = "none")
+        legend.position = "none") +
+   facet_wrap(~Class, scales = "free")  
 
 ics_sums_s <- t %>%
   #transform(Class = factor(Class, levels=c("WUI", "VLD", "Wildlands"))) %>%
