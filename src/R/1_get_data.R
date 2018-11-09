f0c9afdf0017fdfbe730109eb0e35e978a85b5c2
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-11-09 10:19:52 -0700

    moved states and hex in script #2

--- src/R/1_get_data.R
+++ src/R/1_get_data.R
@@ -2,8 +2,8 @@ source("src/R/functions/helper_functions.R")
 source("src/R/functions/st_par.R")
 source("src/R/functions/st_parallel.R")
 
-x <- c("data.table", "tidyverse", "tidyverse", "magrittr", "sf", "gridExtra", "raster", "lme4", 'lubridate',
-       "assertthat", "purrr", "httr", "rvest", "lubridate", "parallel", "broom")
+x <- c("data.table", "tidyverse", "tidyverse", "magrittr", "sf", "gridExtra", "raster", "lme4",
+       'lubridate', "assertthat", "purrr", "httr", "rvest", "lubridate", "parallel", "broom")
 lapply(x, library, character.only = TRUE, verbose = FALSE)
 
 ## Download and process State data
@@ -14,7 +14,6 @@ us_prefix <- file.path(raw_prefix, "cb_2016_us_state_5m")
 bounds_dir <- file.path(prefix, 'bounds')
 ics_prefix <- file.path(prefix, "ics_209")
 ics_inputs <- file.path(ics_prefix, "input_tbls")
-ics_latlong <- file.path(ics_inputs, "latlong")
 ics_spatial <- file.path(ics_prefix, "spatial")
 ecoregion_prefix <- file.path(raw_prefix, "ecoreg")
 counties_prefix <- file.path(raw_prefix, 'cb_2016_us_county_20m')
@@ -58,26 +57,4 @@ if (!file.exists(counties_shp)) {
   unzip(dest, exdir = counties_prefix)
   unlink(dest)
   assert_that(file.exists(counties_shp))
-}
-
-# Import USA states
-states <- st_read(dsn = us_shp, quiet= TRUE) %>%
-  st_transform(proj_ea) %>%  # e.g. US National Atlas Equal Area
-  filter(!(NAME %in% c("Alaska", "Hawaii", "Puerto Rico",
-                       "Commonwealth of the Northern Mariana Islands", "United States Virgin Islands",
-                       "American Samoa", "Guam"))) %>%
-  mutate(state.id = STATEFP, 
-         state.abv = STUSPS,
-         state = NAME,
-         state_km2 = as.numeric(st_area(geometry))/1000000) %>%
-  dplyr::select(state.id, state.abv, state, state_km2) 
-
-if (!file.exists(file.path(bounds_dir, 'hex_grid_50k.gpkg'))) {
-  hex_points <- spsample(as(states, 'Spatial'), type = "hexagonal", cellsize = 50000)
-  hex_grid <- HexPoints2SpatialPolygons(hex_points, dx = 50000)
-  hexnet_50k <- st_as_sf(hex_grid) %>%
-    mutate(hexid50k = row_number()) %>%
-    st_intersection(., st_union(states))
-} else {
-  hexnet_50k <- st_read(file.path(bounds_dir, 'hex_grid_50k.gpkg'))
 }
