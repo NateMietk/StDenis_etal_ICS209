d803b16ca21be0fd529e7264449436b7c85072e0
‘NateMietk’ <natemietkiewicz@gmail.com>
2018-11-30 14:45:01 -0700

    updated using function to make all plots

--- src/figures/figure_2.R
+++ src/figures/figure_2.R
@@ -1,84 +1,25 @@
+conus_mask <- states %>%
+  dplyr::filter(stusps != 'AK') 
 
 figure_2_df <- conus_209 %>%
+  dplyr::filter(stusps != 'AK') %>%
   group_by(hexid50k) %>%
   summarise(n = n(),
-            structures_destroyed = sum(DESTROYED_RES_TOTAL, na.rm = TRUE),
-            total_personnel = sum(TOTAL_PERSONNEL, na.rm = TRUE),
-            burned_area_acres = sum(ACRES, na.rm = TRUE),
-            costs = sum(EST_IM_COST_TO_DATE, na.rm = TRUE),
-            total_threatened = sum(THREATENED_TOTAL, na.rm = TRUE)) %>%
+            structures_destroyed = log(sum(TOTAL_RES_DESTROYED, na.rm = TRUE)),
+            total_personnel = log(sum(TOTAL_PERSONNEL, na.rm = TRUE)),
+            burned_area_acres = log(sum(ACRES, na.rm = TRUE)),
+            costs = log(sum(EST_IM_COST_TO_DATE, na.rm = TRUE)),
+            total_threatened = log(sum(TOTAL_RES_THREATENED, na.rm = TRUE))) %>%
   as.data.frame() %>%
   dplyr::select(-geom) %>%
   left_join(hexnet_50k, ., by = 'hexid50k') %>%
   na.omit(n)
 
-p1 <- ggplot() +
-  geom_sf(data = figure_2_df, aes(fill = log(costs)), color = NA, lwd = 0) +
-  scale_fill_distiller('log(Costs)', palette = 'BrBG', na.value = NA) +
-  geom_sf(data = states, color = "black", lwd=0.1, fill=NA) +
-  coord_sf(crs = st_crs(states), datum = NA) + 
-  theme(
-    panel.ontop = TRUE,   ## Note: this is to make the panel grid visible in this example
-    panel.grid = element_blank(), 
-    line = element_blank(), 
-    rect = element_blank(), 
-    plot.background = element_blank())
-ggsave(file = file.path('results', 'var_hexagonal', "log_costs.pdf"), p1, width = 8, height = 5, 
-       dpi = 600, scale = 3, units = "cm") #saves g
+p1 <- make_map(figure_2_df, 'n', '(A) log(# Events)', 'log(# Events)', conus_mask)
+p2 <- make_map(figure_2_df, 'burned_area_acres', '(B) log(Burned Area)', 'log(Burned Area)', conus_mask) 
+p3 <- make_map(figure_2_df, 'total_personnel', '(C) log(Total Personnel)', 'log(Total Personnel)', conus_mask)
+p4 <- make_map(figure_2_df, 'costs', '(D) log(Costs)', 'log(Costs)', conus_mask)
+p5 <- make_map(figure_2_df, 'structures_destroyed', '(E) log(Homes Destroyed)', 'log(Homes Destroyed)', conus_mask)
+p6 <- make_map(figure_2_df, 'total_threatened', '(F) log(Homes Threatened)', 'log(Homes Threatened)', conus_mask)
 
-p2 <- ggplot() +
-  geom_sf(data = figure_2_df, aes(fill = log(structures_destroyed)), color = NA, lwd = 0) +
-  scale_fill_distiller('log(Structures Destroyed)', palette = 'BrBG', na.value = NA) +
-  geom_sf(data = states, color = "black", lwd=0.1, fill=NA) +
-  coord_sf(crs = st_crs(states), datum = NA) + 
-  theme(
-    panel.ontop = TRUE,   ## Note: this is to make the panel grid visible in this example
-    panel.grid = element_blank(), 
-    line = element_blank(), 
-    rect = element_blank(), 
-    plot.background = element_blank())
-ggsave(file = file.path('results', 'var_hexagonal', "log_structures_destroyed.pdf"), p2, width = 8, height = 5, 
-       dpi = 600, scale = 3, units = "cm") #saves g
-
-p3 <- ggplot() +
-  geom_sf(data = figure_2_df, aes(fill = log(total_personnel)), color = NA, lwd = 0) +
-  scale_fill_distiller('log(Total Personnel)', palette = 'BrBG', na.value = NA) +
-  geom_sf(data = states, color = "black", lwd=0.1, fill=NA) +
-  coord_sf(crs = st_crs(states), datum = NA) + 
-  theme(
-    panel.ontop = TRUE,   ## Note: this is to make the panel grid visible in this example
-    panel.grid = element_blank(), 
-    line = element_blank(), 
-    rect = element_blank(), 
-    plot.background = element_blank())
-ggsave(file = file.path('results', 'var_hexagonal', "log_total_personnel.pdf"), p3, width = 8, height = 5, 
-       dpi = 600, scale = 3, units = "cm") #saves g
-
-p4 <- ggplot() +
-  geom_sf(data = figure_2_df, aes(fill = log(burned_area_acres)), color = NA, lwd = 0) +
-  scale_fill_distiller('log(Burned Area); acres', palette = 'BrBG', na.value = NA) +
-  geom_sf(data = states, color = "black", lwd=0.1, fill=NA) +
-  coord_sf(crs = st_crs(states), datum = NA) + 
-  theme(
-    panel.ontop = TRUE,   ## Note: this is to make the panel grid visible in this example
-    panel.grid = element_blank(), 
-    line = element_blank(), 
-    rect = element_blank(), 
-    plot.background = element_blank())
-ggsave(file = file.path('results', 'var_hexagonal', "log_burned_area_acres.pdf"), p4, width = 8, height = 5, 
-       dpi = 600, scale = 3, units = "cm") #saves g
-
-
-p5 <- ggplot() +
-  geom_sf(data = figure_2_df, aes(fill = log(total_threatened)), color = NA, lwd = 0) +
-  scale_fill_distiller('log(Total Threatened)', palette = 'BrBG', na.value = NA) +
-  geom_sf(data = states, color = "black", lwd=0.1, fill=NA) +
-  coord_sf(crs = st_crs(states), datum = NA) + 
-  theme(
-    panel.ontop = TRUE,   ## Note: this is to make the panel grid visible in this example
-    panel.grid = element_blank(), 
-    line = element_blank(), 
-    rect = element_blank(), 
-    plot.background = element_blank())
-ggsave(file = file.path('results', 'var_hexagonal', "log_total_threatened.pdf"), p5, width = 8, height = 5, 
-       dpi = 600, scale = 3, units = "cm") #saves g
+grid.arrange(p1, p2, p3, p4, p5, p6, ncol = 2)
