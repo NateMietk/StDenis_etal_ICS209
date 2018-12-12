69dc1b52e75fab8a71a70c65d756c330d86b77bc
Your Name <natemietkiewicz@gmail.com>
2018-12-12 16:36:00 -0700

    added gaccs

--- src/R/2_prep_spatial_data.R
+++ src/R/2_prep_spatial_data.R
@@ -7,6 +7,50 @@ states <- st_read(dsn = us_shp, quiet= TRUE) %>%
   rename_all(tolower) %>%
   dplyr::select(stusps) 
 
+# Import and clean by GACC
+# Annoyingly I have to sign into ArcGIS Online to download the data
+# https://hub.arcgis.com/items/72213d9266eb4aefa4403a1bf21dfd61?geometry=-266.765%2C-40.581%2C93.235%2C86.606
+if(!file.exists(file.path(fire_dir, 'mtbs.gpkg'))) {
+  gacc <- st_read(dsn = gacc_prefix, layer = 'National_GACC_2018', quiet= TRUE) %>%
+    st_transform(st_crs(states)) %>%
+    st_intersection(., st_union(states)) %>%
+    rename_all(tolower) %>%
+    dplyr::select(gacc_label, gacc_name)
+  
+  st_write(mtbs, file.path(bounds_dir, 'gacc.gpkg'), delete_layer = TRUE)
+  
+} else {
+  gacc <- st_read(file.path(bounds_dir, 'gacc.gpkg'))
+}
+
+# Import and clean the MTBS polygons
+if(!file.exists(file.path(fire_dir, 'mtbs.gpkg'))) {
+  
+  mtbs_shp <- file.path(raw_dir_mtbs, 'mtbs_perims_DD.shp')
+  if (!file.exists(mtbs_shp)) {
+    dest <- paste0(raw_dir_mtbs, ".zip")
+    download.file("https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/MTBS_Fire/data/composite_data/burned_area_extent_shapefile/mtbs_perimeter_data.zip", dest)
+    unzip(dest, exdir = raw_dir_mtbs)
+    unlink(dest)
+    assert_that(file.exists(mtbs_shp))
+  }
+  
+  mtbs <- st_read(dsn = raw_dir_mtbs, layer = 'mtbs_perims_DD', quiet= TRUE) %>%
+    st_transform(st_crs(states)) %>%
+    mutate(discovery_date = ymd(paste(Year, StartMonth, StartDay, sep="-")),
+           discovery_year = year(discovery_date),
+           discovery_day = day(discovery_date),
+           discovery_month = month(discovery_date),
+           discovery_doy = yday(discovery_date)) %>%
+    st_intersection(., st_union(states)) %>%
+    rename_all(tolower) %>%
+    dplyr::select(fire_id, fire_name, discovery_date, discovery_year, 
+                  discovery_day, discovery_month, discovery_doy, acres)
+  st_write(mtbs, file.path(fire_dir, 'mtbs.gpkg'), delete_layer = TRUE)
+} else {
+  mtbs <- st_read(file.path(fire_dir, 'mtbs.gpkg'))
+}
+
 
 if (!exists("ecoregions_l3")){
   ecoregions_l3 <- st_read(file.path(ecoregion_prefix, 'us_eco_l3.shp')) %>%
@@ -35,10 +79,10 @@ if (!file.exists(file.path(bounds_dir, 'hex_grid_50k.gpkg'))) {
 # Clip the ICS-209 data to the CONUS and remove unknown cause
 if(!file.exists(file.path(ics_spatial, "ics209_conus.gpkg"))) {
   # Import ICS-209 from 1999-2014
-  ics_209 <- fread(file.path(ics_inputs, "ics209_allsitreps1999to2014.csv")) %>%
+  ics_209 <- fread(file.path(ics_inputs, "ics209_allWFincidents1999to2014.csv")) %>%
     as_tibble() %>%
-    mutate(POO_LONGITUDE = as.numeric(POO_LONGITUDE),
-           POO_LATITUDE = as.numeric(POO_LATITUDE))
+    mutate(POO_LONGITUDE = ifelse(is.na(POO_LONGITUDE),0,as.numeric(POO_LONGITUDE)),
+           POO_LATITUDE = ifelse(is.na(POO_LATITUDE),0,as.numeric(POO_LATITUDE)))
   
   # Make the cleaned ICS-209 data spatial
   ics_209_pt <- st_as_sf(ics_209, coords = c("POO_LONGITUDE", "POO_LATITUDE"),
@@ -48,6 +92,7 @@ if(!file.exists(file.path(ics_spatial, "ics209_conus.gpkg"))) {
   conus_209 <- ics_209_pt %>%
     st_intersection(., states) %>%
     st_join(., ecoregions_l3) %>%
+    st_join(., gacc) %>%
     st_join(., hexnet_50k)
   
   st_write(conus_209, file.path(ics_spatial, "ics209_conus.gpkg"), delete_layer=TRUE)
