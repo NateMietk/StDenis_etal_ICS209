4603b1120e35099683e6d3e22fb96080c5998c38
Your Name <natemietkiewicz@gmail.com>
2018-12-12 16:34:35 -0700

    added gaccs

--- src/tables/create_summary_tables.R
+++ src/tables/create_summary_tables.R
@@ -2,8 +2,9 @@
 wb <- createWorkbook()
 
 # Add a sheet for each table
-addWorksheet(wb, "Table 1")
-addWorksheet(wb, "Table 2")
+addWorksheet(wb, "Table 1 - Ecoregion")
+addWorksheet(wb, "Table 2 - States")
+addWorksheet(wb, "Table 3 - GACC")
 
 # Summarize the key 209 metrics based on Level 1 Ecoregions
 eco_reg_table <- conus_209 %>%
@@ -11,12 +12,12 @@ eco_reg_table <- conus_209 %>%
   as.data.frame() %>%
   group_by(na_l1name) %>%
   summarise(n = n(),
-            burned_area_acres = (sum(ACRES, na.rm = TRUE)),
-            total_threatened = (sum(TOTAL_RES_THREATENED, na.rm = TRUE)),
-            structures_destroyed = (sum(TOTAL_RES_DESTROYED, na.rm = TRUE)),
-            total_personnel = (sum(TOTAL_PERSONNEL, na.rm = TRUE)),
-            costs = (sum(EST_IM_COST_TO_DATE, na.rm = TRUE))
-            ) %>%
+            burned_area_acres = log(sum(FINAL_ACRES, na.rm = TRUE)),
+            fire_spread_rate = log(max(WF_MAX_FSR, na.rm = TRUE)),
+            costs = log(sum(PROJECTED_FINAL_IM_COST, na.rm = TRUE)),
+            total_personnel = log(sum(TOTAL_PERSONNEL_SUM, na.rm = TRUE)),
+            total_threatened = log(sum(STR_THREATENED_MAX, na.rm = TRUE)),
+            structures_destroyed = log(sum(STR_DESTROYED_TOTAL, na.rm = TRUE))) %>%
   na.omit()
 
 # Summarize the key 209 metrics based on US states
@@ -24,18 +25,32 @@ state_table <- conus_209 %>%
   as.data.frame() %>%
   group_by(stusps) %>%
   summarise(n = n(),
-            burned_area_acres = (sum(ACRES, na.rm = TRUE)),
-            total_threatened = (sum(TOTAL_RES_THREATENED, na.rm = TRUE)),
-            structures_destroyed = (sum(TOTAL_RES_DESTROYED, na.rm = TRUE)),
-            total_personnel = (sum(TOTAL_PERSONNEL, na.rm = TRUE)),
-            costs = (sum(EST_IM_COST_TO_DATE, na.rm = TRUE))
-  ) %>%
+            burned_area_acres = log(sum(FINAL_ACRES, na.rm = TRUE)),
+            fire_spread_rate = log(max(WF_MAX_FSR, na.rm = TRUE)),
+            costs = log(sum(PROJECTED_FINAL_IM_COST, na.rm = TRUE)),
+            total_personnel = log(sum(TOTAL_PERSONNEL_SUM, na.rm = TRUE)),
+            total_threatened = log(sum(STR_THREATENED_MAX, na.rm = TRUE)),
+            structures_destroyed = log(sum(STR_DESTROYED_TOTAL, na.rm = TRUE))) %>%
+  na.omit()
+
+# Summarize the key 209 metrics based on GACCs
+gacc_table <- conus_209 %>%
+  as.data.frame() %>%
+  group_by(gacc_label) %>%
+  summarise(n = n(),
+            burned_area_acres = log(sum(FINAL_ACRES, na.rm = TRUE)),
+            fire_spread_rate = log(max(WF_MAX_FSR, na.rm = TRUE)),
+            costs = log(sum(PROJECTED_FINAL_IM_COST, na.rm = TRUE)),
+            total_personnel = log(sum(TOTAL_PERSONNEL_SUM, na.rm = TRUE)),
+            total_threatened = log(sum(STR_THREATENED_MAX, na.rm = TRUE)),
+            structures_destroyed = log(sum(STR_DESTROYED_TOTAL, na.rm = TRUE))) %>%
   na.omit()
 
 # Write out the tables into the workbook and relative sheet
-writeData(wb, sheet = "Table 1", x = eco_reg_table)
-writeData(wb, sheet = "Table 2", x = state_table)
+writeData(wb, sheet = "Table 1 - Ecoregion", x = eco_reg_table)
+writeData(wb, sheet = "Table 2 - States", x = state_table)
+writeData(wb, sheet = "Table 3 - GACC", x = gacc_table)
 
 # Save the workbook to the excel file
 saveWorkbook(wb, file = file.path('results', 'draft_tables', "tables_raw.xlsx"),
-             overwrite = T)
+             overwrite = TRUE)
