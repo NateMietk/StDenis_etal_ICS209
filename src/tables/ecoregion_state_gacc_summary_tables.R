3cc2cf250d5a8059754ac3ea4130b79df6c25934
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-04-25 17:11:27 -0600

    fixed variable case

--- src/tables/ecoregion_state_gacc_summary_tables.R
+++ src/tables/ecoregion_state_gacc_summary_tables.R
@@ -12,13 +12,13 @@ eco_reg_table <- conus_209 %>%
   as.data.frame() %>%
   group_by(na_l1name) %>%
   summarise(n = n(),
-            burned_area_acres = sum(FINAL_ACRES, na.rm = TRUE),
-            mean_fire_size = mean(FINAL_ACRES, na.rm = TRUE),
-            meanMAX_fire_spread_rate = mean(WF_MAX_FSR, na.rm = TRUE),
-            costs = sum(PROJECTED_FINAL_IM_COST, na.rm = TRUE),
-            total_personnel = sum(TOTAL_PERSONNEL_SUM, na.rm = TRUE),
-            total_threatened = sum(STR_THREATENED_MAX, na.rm = TRUE),
-            structures_destroyed = sum(STR_DESTROYED_TOTAL, na.rm = TRUE)) %>%
+            burned_area_acres = sum(final_acres, na.rm = TRUE),
+            mean_fire_size = mean(final_acres, na.rm = TRUE),
+            meanMAX_fire_spread_rate = mean(wf_max_fsr, na.rm = TRUE),
+            costs = sum(projected_final_im_cost, na.rm = TRUE),
+            total_personnel = sum(total_personnel_sum, na.rm = TRUE),
+            total_threatened = sum(str_threatened_max, na.rm = TRUE),
+            structures_destroyed = sum(str_destroyed_total, na.rm = TRUE)) %>%
   na.omit()
 
 # Summarize the key 209 metrics based on US states
@@ -26,13 +26,13 @@ state_table <- conus_209 %>%
   as.data.frame() %>%
   group_by(stusps) %>%
   summarise(n = n(),
-            burned_area_acres = sum(FINAL_ACRES, na.rm = TRUE),
-            mean_fire_size = mean(FINAL_ACRES, na.rm = TRUE),
-            meanMAX_fire_spread_rate = mean(WF_MAX_FSR, na.rm = TRUE),
-            costs = sum(PROJECTED_FINAL_IM_COST, na.rm = TRUE),
-            total_personnel = sum(TOTAL_PERSONNEL_SUM, na.rm = TRUE),
-            total_threatened = sum(STR_THREATENED_MAX, na.rm = TRUE),
-            structures_destroyed = sum(STR_DESTROYED_TOTAL, na.rm = TRUE)) %>%
+            burned_area_acres = sum(final_acres, na.rm = TRUE),
+            mean_fire_size = mean(final_acres, na.rm = TRUE),
+            meanMAX_fire_spread_rate = mean(wf_max_fsr, na.rm = TRUE),
+            costs = sum(projected_final_im_cost, na.rm = TRUE),
+            total_personnel = sum(total_personnel_sum, na.rm = TRUE),
+            total_threatened = sum(str_threatened_max, na.rm = TRUE),
+            structures_destroyed = sum(str_destroyed_total, na.rm = TRUE)) %>%
   na.omit()
 
 # Summarize the key 209 metrics based on GACCs
@@ -40,13 +40,13 @@ gacc_table <- conus_209 %>%
   as.data.frame() %>%
   group_by(gacc_label) %>%
   summarise(n = n(),
-            burned_area_acres = sum(FINAL_ACRES, na.rm = TRUE),
-            mean_fire_size = mean(FINAL_ACRES, na.rm = TRUE),
-            meanMAX_fire_spread_rate = mean(WF_MAX_FSR, na.rm = TRUE),
-            costs = sum(PROJECTED_FINAL_IM_COST, na.rm = TRUE),
-            total_personnel = sum(TOTAL_PERSONNEL_SUM, na.rm = TRUE),
-            total_threatened = sum(STR_THREATENED_MAX, na.rm = TRUE),
-            structures_destroyed = sum(STR_DESTROYED_TOTAL, na.rm = TRUE)) %>%
+            burned_area_acres = sum(final_acres, na.rm = TRUE),
+            mean_fire_size = mean(final_acres, na.rm = TRUE),
+            meanMAX_fire_spread_rate = mean(wf_max_fsr, na.rm = TRUE),
+            costs = sum(projected_final_im_cost, na.rm = TRUE),
+            total_personnel = sum(total_personnel_sum, na.rm = TRUE),
+            total_threatened = sum(str_threatened_max, na.rm = TRUE),
+            structures_destroyed = sum(str_destroyed_total, na.rm = TRUE)) %>%
   na.omit()
 
 # Write out the tables into the workbook and relative sheet
@@ -55,5 +55,5 @@ writeData(wb, sheet = "Table 2 - States", x = state_table)
 writeData(wb, sheet = "Table 3 - GACC", x = gacc_table)
 
 # Save the workbook to the excel file
-saveWorkbook(wb, file = file.path('results', 'draft_tables', "tables_raw.xlsx"),
+saveWorkbook(wb, file = file.path(draft_table_dir, "ecoregion_state_gacc_comparison.xlsx"),
              overwrite = TRUE)
