060ee37e74fe851958010b612caac4166c18a44b
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-12-12 17:02:26 -0700

    lower case

--- src/figures/figure_S1.R
+++ src/figures/figure_S1.R
@@ -16,12 +16,12 @@ figure_S1_df <- conus_209 %>%
   left_join(hexnet_50k, ., by = 'hexid50k') %>%
   na.omit(n)
 
-p1 <- make_map(figure_S1_df, 'fsr', '(A) Fire spread rate', 'log(Max FSR (acres/day))', ak_mask) 
-p2 <- make_map(figure_S1_df, 'burned_area_acres', '(B) Burned Area', 'log(Burned Area (acres))', ak_mask) 
-p3 <- make_map(figure_S1_df, 'costs', '(D) Costs', 'log(Costs ($))', ak_mask)
-p4 <- make_map(figure_S1_df, 'total_personnel', '(C) Total Personnel', 'log(Total Personnel)', ak_mask)
-p5 <- make_map(figure_S1_df, 'total_threatened', '(F) Homes Threatened', 'log(Homes Threatened)', ak_mask)
-p6 <- make_map(figure_S1_df, 'structures_destroyed', '(E) Homes Destroyed', 'log(Homes Destroyed)', ak_mask)
+p1 <- make_map(figure_S1_df, 'fsr', '(a) Fire spread rate', 'log(Max FSR (acres/day))', ak_mask) 
+p2 <- make_map(figure_S1_df, 'burned_area_acres', '(b) Burned Area', 'log(Burned Area (acres))', ak_mask) 
+p3 <- make_map(figure_S1_df, 'costs', '(d) Costs', 'log(Costs ($))', ak_mask)
+p4 <- make_map(figure_S1_df, 'total_personnel', '(c) Total Personnel', 'log(Total Personnel)', ak_mask)
+p5 <- make_map(figure_S1_df, 'total_threatened', '(f) Homes Threatened', 'log(Homes Threatened)', ak_mask)
+p6 <- make_map(figure_S1_df, 'structures_destroyed', '(e) Homes Destroyed', 'log(Homes Destroyed)', ak_mask)
 
 g <- arrangeGrob(p1, p2, p3, p4, p5, p6, ncol = 2)
 
